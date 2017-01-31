89204cf1a13b4f00d9f6199ce9de60807dc80698
Scott Woodend <woodendscott@gmail.com>
2017-01-31 17:35:29 -0500

    add test to / route

--- tests/route-tests.js
+++ tests/route-tests.js
@@ -5,10 +5,18 @@ const request = require('supertest');
 const app = require('../lib/server.js');
 
 
+
 describe('access "/" route', function(){
   it('should respond with a 200', function(done){
     request(app)
       .get('/')
       .expect(200, done)
+  });
+
+  it('should send the landing page html file', function(done){
+    request(app)
+      .get('/')
+      .expect('Content-Length', "518")
+      .expect('Content-Type', /html/, done)
   });
 });
