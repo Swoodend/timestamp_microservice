0facb774ff726d2fa6c0dca04e818b6ed19a5284
Scott Woodend <woodendscott@gmail.com>
2017-01-31 23:41:04 -0500

    set up test to determine if user passed unix or natural date

--- tests/route-tests.js
+++ tests/route-tests.js
@@ -3,10 +3,11 @@ const chai = require('chai');
 const expect = chai.expect;
 const request = require('supertest');
 const app = require('../lib/server.js');
+const dateTime = require('../lib/helpers/datetime.js');
 
 
 
-describe('access "/" route', function(){
+describe('accessing the "/" route', function(){
   it('should respond with a 200', function(done){
     request(app)
       .get('/')
@@ -18,5 +19,16 @@ describe('access "/" route', function(){
       .get('/')
       .expect('Content-Length', "518")
       .expect('Content-Type', /html/, done)
+  });
+});
+
+describe('test the unixOrNatural function', function(){
+  it ('should correctly identify if date is unix or natural', function(done){
+    let date1 = '1234567812';
+    let date2 = 'December%2020,%202015';
+    let result1 = dateTime.unixOrNatural(date1);
+    let result2 = dateTime.unixOrNatural(date2);
+    expect(result1).to.eql('unix');
+    expect(result2).to.eql('natural');
   });
 });
