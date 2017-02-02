e8daef4777789f14b6598a0f0ae4fe6d4da88603
Scott Woodend <woodendscott@gmail.com>
2017-02-02 12:31:00 -0500

    renamed entry file app.js

--- tests/route-tests.js
+++ tests/route-tests.js
@@ -2,7 +2,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 const request = require('supertest');
-const app = require('../lib/server.js');
+const app = require('../lib/app.js');
 const dateTime = require('../lib/helpers/datetime.js');
 
 describe('accessing the "/" route', function(){
