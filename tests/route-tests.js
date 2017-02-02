e9a28fedfbc3e4d25ec762907b009ba8ef232879
Scott Woodend <woodendscott@gmail.com>
2017-02-02 00:50:18 -0500

    update tests to check api response scenarios

--- tests/route-tests.js
+++ tests/route-tests.js
@@ -5,7 +5,10 @@ const request = require('supertest');
 const app = require('../lib/server.js');
 const dateTime = require('../lib/helpers/datetime.js');
 const timestamp = require('unix-timestamp');
+const moment = require('moment');
 
+//some of these tests do not apply anymore as I found out about the moment.js library
+//halfway through the project, good practice either way...
 
 describe('accessing the "/" route', function(){
   it('should respond with a 200', function(done){
@@ -19,6 +22,59 @@ describe('accessing the "/" route', function(){
       .get('/')
       .expect('Content-Length', "518")
       .expect('Content-Type', /html/, done)
+  });
+});
+
+describe('accessing the "/:date" route', function(){
+  it('should respond with json string indicating unix/natural time given unix format', function(done){
+     request(app)
+      .get('/1646197200')
+      .expect(200)
+      .end(function(err, res){
+        expect(res.text).to.eql('{"unix":"1646197200","natural":"March 02, 2022"}');
+        done();
+      })
+  });
+
+  it('should respond with json string indicating unix/natural time given natural format', function(done){
+    request(app)
+      .get('/march 02, 2022')
+      .expect(200)
+      .end(function(err, res){
+        expect(res.text).to.eql('{"unix":"1646197200","natural":"March 02, 2022"}')
+        done();
+      })
+  });
+
+  it('should respond with null given bad user input', function(done){
+    request(app)
+      .get('/akjhfaklfjah23432')
+      .expect(200)
+      .end(function(err, res){
+        expect(res.text).to.eql('{"unix":'+null+',"natural":'+null+'}');
+      });
+
+    request(app)
+      .get('/february 31 2017')
+      .expect(200)
+      .end(function(err, res){
+        expect(res.text).to.eql('{"unix":'+null+',"natural":'+null+'}');
+      });
+
+      request(app)
+      .get('/jans 21, 2013')
+      .expect(200)
+      .end(function(err, res){
+        expect(res.text).to.eql('{"unix":'+null+',"natural":'+null+'}');
+      });
+
+      request(app)
+      .get('/jans 21 2013')
+      .expect(200)
+      .end(function(err, res){
+        expect(res.text).to.eql('{"unix":'+null+',"natural":'+null+'}');
+        done();
+      });
   });
 });
 
